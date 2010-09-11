7a8db3945c6926e26f1ce298d15b11520e5856cb
Simone Economo <my.ekoes@gmail.com>
2010-09-11 02:32:39 +0200

    Italian language

---
+++ lang/lang.it.php
@@ -0,0 +1,25 @@
+<?php
+
+	$about = array(
+		'name' => 'Italiano',
+		'author' => array(
+			'name' => 'Simone Economo',
+			'email' => 'my.ekoes@gmail.com',
+			'website' => 'http://lineheight.net',
+		),
+	);
+	
+	
+	/*
+	 * EXTENSION: Publish Tabs
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+
+		'Publish Tab' =>
+		'Scheda'
+
+	);
+
+?>
