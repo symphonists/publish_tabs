1651dfd0ca472bdf8a96a2bf1863dd27c0b4820a
Nils Hörrmann <post@nilshoerrmann.de>
2011-02-16 01:17:34 +0800

    German translation

---
+++ lang/lang.de.php
@@ -0,0 +1,24 @@
+<?php
+
+	$about = array(
+		'name' => 'Deutsch',
+		'author' => array(
+			'name' => 'Nils Hörrmann',
+			'email' => 'post@nilshoerrmann.de',
+			'website' => 'http://nilshoerrmann.de'
+		),
+		'release-date' => '2011-02-15'
+	);
+
+	/**
+	 * Publish Tabs
+	 */
+	$dictionary = array(
+
+		'Untitled Tab' => 
+		'Ohne Titel',
+
+		'Publish Tab' => 
+		'Reiter',
+
+	);
