f8ee825619f8fa317377cbe064226b1199d4fecb
Alexandra Nantel <alexandra@deuxhuithuit.com>
2018-07-24 16:55:42 -0400

    Performance update.

--- fields/field.publish_tabs.php
+++ fields/field.publish_tabs.php
@@ -20,29 +20,12 @@
 	-------------------------------------------------------------------------*/
 
 		public function createTable(){
-			return Symphony::Database()
-				->create('tbl_entries_data_' . $this->get('id'))
-				->ifNotExists()
-				->charset('utf8')
-				->collate('utf8_unicode_ci')
-				->fields([
-					'id' => [
-						'type' => 'int(11)',
-						'auto' => true,
-					],
-					'entry_id' => 'int(11)',
-					'value' => [
-						'type' => 'double',
-						'null' => true,
-					],
-				])
-				->keys([
-					'id' => 'primary',
-					'entry_id' => 'key',
-					'value' => 'key',
-				])
-				->execute()
-				->success();
+			return false;
+		}
+
+		public function requiresTable()
+		{
+			return false;
 		}
 
 	/*-------------------------------------------------------------------------
@@ -90,20 +73,28 @@
 
 		}
 
-		public function prepareReadableValue($data, $entry_id = NULL, $truncate = false, $defaultValue = NULL) {
+		public function prepareReadableValue($data, $entry_id = null, $truncate = false, $defaultValue = null) {
 			return $this->prepareTableValue($data, null, $entry_id);
 		}
 
-		public function prepareTableValue($data, XMLElement $link=NULL, $entry_id=NULL) {
-			// build this entry fully
-			$entries = (new EntryManager)
-				->select()
+		public function prepareTableValue($data, XMLElement $link = null, $entry_id = null) {
+			if (!$entry_id) {
+				return;
+			}
+			// Fetch entry
+			$entry = (new EntryManager)
+				->select([])
+				->projection(['e.id'])
+				->disableDefaultSort()
 				->entry($entry_id)
+				->section($this->get('parent_section'))
 				->execute()
 				->next();
 
-			if (!$entries) return parent::prepareTableValue(NULL, $link, $entry_id);
-
+			if (!$entry) {
+				return parent::prepareTableValue(null, $link, $entry_id);
+			}
+			// build this entry fully
 			$entry = (new EntryManager)
 				->select()
 				->entry($entry_id)
@@ -119,12 +110,15 @@
 				->where(['parent_section' => $this->get('parent_section')])
 				->where(['sortorder' => ['>' => $this->get('sortorder')]])
 				->where(['show_column' => 'yes'])
+				->where(['id' => ['!=' => $this->get('id')]])
 				->orderBy('sortorder')
 				->limit(1)
 				->execute()
 				->variable('id');
 
-			if (!$field_id) return parent::prepareTableValue(NULL, $link, $entry_id);
+			if (!$field_id) {
+				return parent::prepareTableValue(null, $link, $entry_id);
+			}
 
 			$field = (new FieldManager)
 				->select()
@@ -132,7 +126,7 @@
 				->execute()
 				->next();
 
-			// get the first field's value as a substitude for the tab's return value
+			// get the first field's value as a substitute for the tab's return value
 			return $field->prepareTableValue($entry->getData($field_id), $link, $entry_id);
 		}
 
